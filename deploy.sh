6fb23de9d2ac505ae13469464c6c56202f0b3fd2
Chilipp <p.sommer@posteo.de>
2017-01-06 20:52:42 +0100

    Moved build and compile parts of deploy.sh to .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -2,41 +2,7 @@
 set -e # Exit with nonzero exit code if anything fails
 set -x
 
-SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
-
-function doCompile {
-    pip install sphinx-fortran sphinx==1.3.5 sphinx_rtd_theme ipython sphinx-argparse autodocsumm
-    cd docs
-    sphinx-build . ../out
-    touch ../out/.nojekyll
-    cd ../
-}
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$BUILD_DOCS" != "true" ]; then
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
-fi
-
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean out existing contents
-ls -ad out/.* out/* | grep -v "out/.git$" | grep -v "out/.$" | grep -v "out/..$" | xargs rm -r
-
-# Run our compile script
-doCompile
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
