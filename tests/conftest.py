89bcd293728a3e405d6ece7dff28b7012aa5639a
Chilipp <p.sommer@posteo.de>
2017-01-07 11:21:19 +0100

    Raise warning if offline option already exists

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
+from warnings import warn
+
 
 def pytest_addoption(parser):
     group = parser.getgroup("gwgen", "GWGEN specific options")
-    group.addoption('--offline', help="Block outgoing internet connections",
-                    action='store_true')
+    try:
+        group.addoption('--offline', action='store_true',
+                        help="Block outgoing internet connections")
+    except ValueError:  # offline option already registerd
+        warn("offline option already registered in gwgen group! The "
+             "registered options are %s" % group.options)
+        pass
     group.addoption('--no-db', help="Don't test postgres databases",
                     action='store_true')
     group.addoption('--database',
