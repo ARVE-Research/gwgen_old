985950c5d4923a3487ac9f42eaef866737374c40
Chilipp <p.sommer@posteo.de>
2017-02-15 17:37:00 +0100

    Minor changes

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,13 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+set +x
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+set -x
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
