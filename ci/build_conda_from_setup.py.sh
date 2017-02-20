bfbe992d9c27bc657dfa7c939f305aa169b18863
Chilipp <p.sommer@posteo.de>
2017-02-20 18:30:04 +0100

    Turned ci/build_conda_from_setup.py.sh into a function

--- ci/build_conda_from_setup.py.sh
+++ ci/build_conda_from_setup.py.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-set -xe
 
-DIR=$1
+function build_conda() {
 
-cd $DIR
-# build the tarball
-python setup.py sdist
-FNAME=`ls -rt dist/* | tail -n 1`
-# create the receipt
-mkdir tmp
-RUNNING_SKELETON=1 conda skeleton pypi --output-dir tmp --python-version ${PYTHON_VERSION} \
-    --manual-url file://`pwd`/${FNAME}
-# build
-cd tmp
-conda build --python ${PYTHON_VERSION} `ls .`
-# print the output
-FNAME=$(conda build --python ${PYTHON_VERSION} `ls .` --output)
-cd ../
-rm -rf tmp
+    DIR=$1
 
-echo $FNAME
+    WORK=`pwd`
+
+    cd $DIR
+    # build the tarball
+    python setup.py sdist
+    FNAME=`ls -rt dist/* | tail -n 1`
+    # create the receipt
+    mkdir tmp
+    RUNNING_SKELETON=1 conda skeleton pypi --output-dir tmp --python-version ${PYTHON_VERSION} \
+        --manual-url file://`pwd`/${FNAME}
+    # build
+    cd tmp
+    conda build --python ${PYTHON_VERSION} `ls .`
+    # print the output
+    BUILD_FNAME=$(conda build --python ${PYTHON_VERSION} `ls .` --output)
+    cd ../
+    rm -rf tmp
+    cd $WORK
+}
