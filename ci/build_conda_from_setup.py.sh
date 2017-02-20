af4594697b834cbb44c730819f66ee16a1ae30cd
Chilipp <p.sommer@posteo.de>
2017-02-20 12:14:32 +0100

    Minor changes

--- ci/build_conda_from_setup.py.sh
+++ ci/build_conda_from_setup.py.sh
@@ -8,7 +8,7 @@ python setup.py sdist
 FNAME=`ls -rt dist/* | tail -n 1`
 # create the receipt
 mkdir tmp
-conda skeleton pypi --output-dir tmp --manual-url file://`pwd`/${FNAME}
+conda skeleton pypi --output-dir tmp --python-version ${PYTHON_VERSION} --manual-url file://`pwd`/${FNAME}
 # build
 cd tmp
 conda build --python ${PYTHON_VERSION} `ls .`
